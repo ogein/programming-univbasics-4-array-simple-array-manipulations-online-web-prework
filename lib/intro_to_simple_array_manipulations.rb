a79f263349407df7a4a9af65d6f2be760abc3de0
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-03 01:24:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,8 +27,8 @@ def using_concat(array1,array2)
   return array1.concat(array2)
 end
   
-def using_insert
-
+def using_insert(array,element)
+  return array.insert(4, element)
 end
 
 def using_uniq
